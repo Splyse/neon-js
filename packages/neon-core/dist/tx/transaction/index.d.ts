706dad705cab4931a36186d73faf60a2152c0fb3
Joe Stewart <gitlab@splyse.tech>
2019-04-14 08:27:26 -0400

    added dist for cdn

---
+++ packages/neon-core/dist/tx/transaction/index.d.ts
@@ -0,0 +1,8 @@
+export * from "./BaseTransaction";
+export * from "./Transaction";
+export * from "./ClaimTransaction";
+export * from "./ContractTransaction";
+export * from "./InvocationTransaction";
+export * from "./StateTransaction";
+export * from "./TransactionType";
+//# sourceMappingURL=index.d.ts.map
